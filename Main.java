b618b3699a81a40f8f2a1c590f907da09d3584b8
Mario Avery Castelo <91093467@g.mvusd.net>
2021-01-11 20:02:38 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+class Main {
+  public static void main(String[] args) 
+  {
+    Scanner kb = new Scanner (System.in);
+    int num1;
+    System.out.print("Pick an int: ");
+    num1 = kb.nextInt();
+    System.out.println("Today's lucky number is " + num1);
+  } // end main method
+} // close the Main class
