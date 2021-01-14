cb6ad6646d88a6d3ae4cd012427ba727bfffb7b7
Mario Castelo <91093467@g.mvusd.net>
2021-01-14 20:06:46 +0000

    Commented out scanner and picking an int section

--- Main.java
+++ Main.java
@@ -3,11 +3,11 @@ import java.util.Scanner;
 class Main {
   public static void main(String[] args) 
   {
-    Scanner kb = new Scanner (System.in);
-    int num1;
-    System.out.print("Pick an int: ");
-    num1 = kb.nextInt();
-    System.out.println("Today's lucky number is " + num1);
+    // Scanner kb = new Scanner (System.in);
+    // int num1;
+    // System.out.print("Pick an int: ");
+    // num1 = kb.nextInt();
+    // System.out.println("Today's lucky number is " + num1);
 
     Apple yuckyApple = new Apple();
     System.out.println(yuckyApple.toString());
